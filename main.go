f68f956d955f18fd0363a4da5032b59fd5d5e0e5
Muh Zakir Ramadhan <mzramadhan@digiasia.asia>
2023-04-16 17:13:38 +0700

    update parse config

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	"github.com/zakirkun/echo-boiler/infrastructure"
 	"github.com/zakirkun/echo-boiler/router"
 )
 
+var cfgFile *string
+
+func init() {
+	cfgFile = flag.String("c", "config.toml", "Configuration")
+}
+
 func main() {
 	// load config
-	cfgBundle := infrastructure.NewConfiguration("config.toml")
+	cfgBundle := infrastructure.NewConfiguration(*cfgFile)
 	cfgBundle.TomlConfiguration()
 
 	application := infrastructure.NewInfrastructure(setServer())
